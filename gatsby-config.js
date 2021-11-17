498af0b1c9af1e1d88b3ff14437077025db64c19
alancphilpott <alanp1995@gmail.com>
2021-11-17 18:36:06 +0000

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,6 @@ module.exports = {
     siteImage: `/banner.jpg`,
     author: `@alancphilpott`,
   },
-  // pathPrefix: "/gatsby-starter-portfolio-cara",
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
